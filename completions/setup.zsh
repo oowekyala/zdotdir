d869985653a106ed8c09a1bc1047daf4b70c80bc
Clément Fournier <clement.fournier@tu-dresden.de>
2025-11-17 16:58:34 +0100

    Add mlir as submodule

--- completions/setup.zsh
+++ completions/setup.zsh
@@ -3,8 +3,3 @@
 
 autoload -Uz _just
 compdef _just just
-
-# Compdef for MLIR
-# Note that some of the options listed are bullshit as they are pass parameters
-# compdef _gnu_generic cinm-opt mlir-opt tilefirst-opt
-compdef _mlir_opt_comp tilefirst-opt mlir-opt cinm-opt
