ff1e6def46a6bf8afa3c98852461499b2515f830
Clément Fournier <clement.fournier@tu-dresden.de>
2026-05-20 01:58:34 +0200

    Refactor just completions

--- completions/setup.zsh
+++ completions/setup.zsh
@@ -1,5 +1,8 @@
 # This dir is put in fpath by zephyr/completions plugin
 # This wrapper script acts as a plugin that defines autoloaded completions without calling compinit
 
+# Per-recipe just completions: add just/ subdir to fpath and autoload all _just_* functions from it
+fpath=("${0:h}/just" $fpath)
+autoload -Uz "${0:h}"/just/_just_*(.:t)
+
 autoload -Uz _just
-compdef _just just
